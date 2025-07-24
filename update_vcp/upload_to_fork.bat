42c3eb3e701b222d7c84eb0cf68e3954b0e4cb0e
ZiChuanShanFeng <2778337567@qq.com>
2025-07-24 20:29:22 +0800

    chore: Sync local changes

--- update_vcp/upload_to_fork.bat
+++ update_vcp/upload_to_fork.bat
@@ -59,11 +59,7 @@ git add -A
 echo.
 
 echo Step 4: Committing all staged files...
-set /p commit_message="Please enter your commit message: "
-if not defined commit_message (
-    echo No commit message entered. Using a default message.
-    set "commit_message=chore: Sync local changes"
-)
+set "commit_message=chore: Sync local changes"
 git commit -m "%commit_message%"
 if %errorlevel% neq 0 (
     echo Warning: Commit failed. This might be because there are no changes to commit.
